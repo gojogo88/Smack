544942bab91a449c8c24244e9fb2cad4ec8683d2
Jonathan Go <jogo88@gmail.com>
2017-08-31 16:43:08 +0900

    added gradient and nav layout

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,23 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Jonathan Go on 2017/08/31.
+//  Copyright © 2017 Appdelight. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        
+    }
+
+
+    
+
+}
