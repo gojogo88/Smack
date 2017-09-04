bdfb5ef585a24b93e1fe297953e259f457b73685
Jonathan Go <jogo88@gmail.com>
2017-09-04 13:27:21 +0900

    avatar picker function added

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -26,6 +26,13 @@ class CreateAccountVC: UIViewController {
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName
+        }
+    }
 
     @IBAction func closeBtnPressed(_ sender: UIButton) {
         performSegue(withIdentifier: UNWIND, sender: nil)
