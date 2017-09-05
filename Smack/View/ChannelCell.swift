e612ed30345a1e682fdecc9b328eaf24c3a0768b
Jonathan Go <jogo88@gmail.com>
2017-09-05 10:23:13 +0900

    can now display channels

---
+++ Smack/View/ChannelCell.swift
@@ -0,0 +1,36 @@
+//
+//  ChannelCell.swift
+//  Smack
+//
+//  Created by Jonathan Go on 2017/09/05.
+//  Copyright © 2017 Appdelight. All rights reserved.
+//
+
+import UIKit
+
+class ChannelCell: UITableViewCell {
+
+    //Outlets
+    @IBOutlet weak var channelName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+        if selected {
+            self.layer.backgroundColor = UIColor(white: 1, alpha: 0.2).cgColor
+        } else {
+            self.layer.backgroundColor = UIColor.clear.cgColor
+        }
+    }
+
+    func configureCell(channel: Channel) {
+        let title = channel.channeltTitle ?? ""
+        channelName.text = "#\(title)"
+    }
+}
